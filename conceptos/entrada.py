cd435787499a2e2214bc5c307ddeefe70e61326f
ErickZH <zaraterick@outlook.com>
2018-01-01 19:34:16 -0600

    Entrada de datos en python.

---
+++ conceptos/entrada.py
@@ -0,0 +1,12 @@
+print("¿Cúal es tu nombre?")
+nombre = input()# retorna un valor del tipo string
+
+print("¿Cúal es tu edad?")
+edad = int(input())
+
+print("¿Cúal es tu peso?")
+peso = float(input())
+
+print("Hola", nombre)
+print("Edad:", edad)
+print("Peso:", peso)
