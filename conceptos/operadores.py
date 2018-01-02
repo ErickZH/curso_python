f3d69b35a8ce2fe989d265f62960b476ce23e70b
ErickZH <zaraterick@outlook.com>
2018-01-01 19:12:47 -0600

    Asignación multiple y operadoes relacionales y logicos.

---
+++ conceptos/operadores.py
@@ -0,0 +1,27 @@
+#Operadores relacionales y logicos
+variable_uno = 19
+variable_dos = 10
+
+mayor = variable_uno > variable_dos
+
+menor = variable_uno < variable_dos
+
+mayor_igual = variable_uno >= variable_dos
+
+menor_igual = variable_uno <= variable_dos
+
+igual = variable_uno == variable_dos
+
+diferente = variable_uno != variable_dos
+
+# print(mayor)
+# print(menor)
+# print(mayor_igual)
+# print(menor_igual)
+# print(igual)
+# print(diferente)
+
+#Operadores logicos AND, OR y NOT
+resultado = False or False or igual
+print(resultado)
+
