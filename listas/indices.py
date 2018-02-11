2edb9b3dcb2f1ce6ef6713cdfc7f0072feb8c042
ErickZH <zaraterick@outlook.com>
2018-02-10 20:17:31 -0600

    Listas, sublistas.

---
+++ listas/indices.py
@@ -0,0 +1,11 @@
+# Listas
+cursos = ['python', 'django', 'flask', 'c', 'php', 'Laravel']
+
+curso = cursos[0]
+
+print(curso)
+
+# sublista
+sub = cursos[0:3]
+
+print(sub)
---
+++ listas/operadores.py
@@ -0,0 +1,9 @@
+lista = [10, 19, 1.2, -3, 13.1415, 290, 1000, -190]
+
+# Ordenar ascendentemente
+lista.sort()
+
+# Ordenar descendentemente
+lista.sort(reverse=True)
+
+print(lista)
